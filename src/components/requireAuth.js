203e2a269c5358e8dd9b4e25d273b157c57f4bc9
ngocthily <ngoctly94@gmail.com>
2021-01-21 13:18:42 -0800

    Create HOC for auth

---
+++ src/components/requireAuth.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default ChildComponent => {
+    class ComposedComponent extends Component {
+        componentDidMount() {
+            this.navigateAway();
+        }
+
+        componentDidUpdate() {
+            this.navigateAway();
+        }
+
+        navigateAway() {
+            if (!this.props.auth) {
+                this.props.history.push('/');
+            }
+        }
+
+        render() {
+            return (
+                <ChildComponent {...this.props}/>
+            )
+        }
+    }
+
+    function mapStateToProps(state) {
+        return { auth: state.auth }
+    }
+
+    return connect(mapStateToProps)(ComposedComponent);
+};
